d37391e18e5cb3131216548ac58eecdc96c93fcc
Markcoma <dex.go@yandex.ru>
2022-02-16 15:56:47 +0300

    direct page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Home from "./components/screens/home/Home";
 import { Route, Routes } from "react-router-dom";
 import { Profile } from "./components/screens/profile/Profile";
+import { Direct } from "./components/screens/direct/Direct";
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path="/home" element={<Home />} />
         <Route path="/profile/*" element={<Profile />} />
         <Route path="/accounts/edit/" element={<> 1231</>} />
+        <Route path="/send" element={<Direct />} />
       </Routes>
     </div>
   );
