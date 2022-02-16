47924f073cf66f9f5ce52f3e0a6c6562776e1155
Markcoma <dex.go@yandex.ru>
2022-02-16 12:27:37 +0300

    onClick "Like"

--- src/components/shared/blockAction/BlockAction.jsx
+++ src/components/shared/blockAction/BlockAction.jsx
@@ -1,21 +1,23 @@
 import React, { useState } from "react";
 import s from "./blockAction.module.css";
 import noLike from "../../../assets/images/heart.png";
+import like from "../../../assets/images/like.svg";
 
-const BlockAction = (props) => {
-  let [isLike, setIsLike] = useState(false); //вместо фолс будет приходить props.isLike
+const BlockAction = () => {
+  let [isLike, setIsLike] = useState(false); //вместо фолс будет приходить props.isLike и дальше будут переработаны обрабочики
 
-  //здесь должен быть метод реагирующий на клик
-  //должен быть метод обрабатывающий onChange
-
+  const onLike = () => {
+    !isLike ? setIsLike(true) : setIsLike(false);
+  };
+  // здесь должен быть onChange который будет сообщать стейту что isLike изменился
   return (
     <div className={s.blockAction}>
       <div>
-        <button className={s.btn}>
+        <button className={s.btn} onClick={onLike}>
           {!isLike ? (
             <img src={noLike} alt="noLike" width="24" />
           ) : (
-            <img src="" alt="like" width="24" />
+            <img src={like} alt="like" width="24" />
           )}
         </button>
         <button className={s.btn}>
