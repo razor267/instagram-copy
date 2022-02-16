7526c538ce3c9206eb8936eedc5d6e74a2ec9e1a
markcoma <dex.go@yandex.ru>
2022-02-16 18:11:03 +0300

    merge

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from "react";
 import "./App.css";
 import Home from "./components/screens/home/Home";
 import { Route, Routes } from "react-router-dom";
+import { Profile } from "./components/screens/profile/Profile";
+import { Direct } from "./components/screens/direct/Direct";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/home" element={<Home />} />
+        <Route path="/profile/*" element={<Profile />} />
+        <Route path="/accounts/edit/" element={<> 1231</>} />
+        <Route path="/send" element={<Direct />} />
       </Routes>
     </div>
   );
