4261668e6c688b4f63297a067ed14060e5954c61
Markcoma <dex.go@yandex.ru>
2022-02-17 16:03:04 +0300

    react-hook-form

--- src/components/shared/writeComment/WriteComment.jsx
+++ src/components/shared/writeComment/WriteComment.jsx
@@ -1,23 +1,29 @@
 import React, { useState } from "react";
+import { useForm } from "react-hook-form";
 import s from "./writeComment.module.css";
 
 const WriteComment = (props) => {
-  let [isEmoji, setIsEmoji] = useState(true); // захардкодил true пока не настроил пропсы, далее будет false а в пропсах будет приходть значение
-
+  let [isEmoji, setIsEmoji] = useState(true); // захардкодил true пока не настроил пропсы, далее будет пропсах будет приходть значение
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   if (props.isEmoji) {
     setIsEmoji(props.isEmoji);
   }
 
   return (
-    <div className={s.writeComment}>
-      {isEmoji ? <button className={s.emojiBtn}>Эмоджи</button> : ""}
-      <textarea
-        className={s.textarea}
-        placeholder="Добавте коментарий..."
-      ></textarea>
-      <button className={s.publichBtn}>Опубликовать</button>{" "}
-      {/* перепишется используя библеотеку с формами*/}
-    </div>
+    <>
+      <form onSubmit={handleSubmit(onSubmit)} className={s.writeComment}>
+        {isEmoji ? <button className={s.emojiBtn}>Эмоджи</button> : ""}
+        <textarea
+          className={s.textarea}
+          placeholder="Добавте коментарий..."
+          {...register("comment")}
+        />
+        <button className={s.publichBtn}>Опубликовать</button>
+      </form>
+    </>
   );
 };
 
