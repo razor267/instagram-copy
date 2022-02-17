db56cfbed74fb1f7bf666129aea4696868bf7503
Markcoma <dex.go@yandex.ru>
2022-02-17 15:48:16 +0300

    misspell

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
   return (
     <header className={s.header}>
       <div className={s.content}>
-        <NavLink href="/home">
+        <NavLink to="/home">
           <img
             src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
             alt="logo"
