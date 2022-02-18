13c5d3598d54ba334c4244ffd70e352e8cd59ee4
Markcoma <dex.go@yandex.ru>
2022-02-18 10:37:53 +0300

    redux toolkit

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,15 +1,13 @@
-import { combineReducers, createStore } from "redux";
 import { directReduser } from "./redusers/directReducer";
 import { postsCardReducer } from "./redusers/postsCardReducer";
 
-const reducers = combineReducers({
-  postsCard: postsCardReducer,
-  directReducer: directReduser,
+import { configureStore } from "@reduxjs/toolkit";
+
+export const store = configureStore({
+  reducer: {
+    postsCard: postsCardReducer,
+    directReducer: directReduser,
+  },
 });
-let store = createStore(reducers);
-
-window.store = store;
-
-export default store;
 
 // утановить ReduxToolkit и использовать его функции, чтобы в дальнейшем уменьшить код
