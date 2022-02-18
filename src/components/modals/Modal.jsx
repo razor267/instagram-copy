f15d9fdc97018a9da9a71682025a30a84923a837
razor267 <razor267@mail.ru>
2022-02-18 22:21:43 +0300

    fix bug render new comment by Greek =))

--- src/components/modals/Modal.jsx
+++ src/components/modals/Modal.jsx
@@ -1,31 +1,44 @@
 import React from "react";
 import s from "./modal.module.css";
+import {ModalCard} from "../shared/card/ModalCard";
 
 export const Modal = ({
-  visible = false,
-  title = "",
-  content = "",
-  isHeader,
-  onClose,
-}) => {
+                        visible = false,
+                        title = "",
+                        // content = "",
+                        isHeader,
+                        onClose,
+                        sendMessageComment,
+                        id,
+                        commentEl,
+                        nickName,
+                        img
+
+                      }) => {
   if (!visible) return null;
   return (
-    <div className={s.modal} onClick={onClose}>
-      <div className={s.dialog} onClick={(e) => e.stopPropagation()}>
-        {isHeader ? (
-          <header className={s.header}>
-            <h3 className={s.title}>{title}</h3>
-            <span className={s.close} onClick={onClose}>
+      <div className={s.modal} onClick={onClose}>
+        <div className={s.dialog} onClick={(e) => e.stopPropagation()}>
+          {isHeader ? (
+              <header className={s.header}>
+                <h3 className={s.title}>{title}</h3>
+                <span className={s.close} onClick={onClose}>
               &times;
             </span>
-          </header>
-        ) : (
-          ""
-        )}
-        <div className={s.body}>
-          <div className={s.content}>{content}</div>
+              </header>
+          ) : (
+              ""
+          )}
+          <div className={s.body}>
+            <div className={s.content}><ModalCard
+                sendMessageComment={sendMessageComment}
+                id={id}
+                commentEl={commentEl}
+                nickName={nickName}
+                img={img}
+            /></div>
+          </div>
         </div>
       </div>
-    </div>
   );
 };
--- src/components/shared/card/Card.jsx
+++ src/components/shared/card/Card.jsx
@@ -10,70 +10,75 @@ import { sendMessageComment } from "../../../redux/redusers/postsCardReducer";
 
 const Card = (props) => {
   const [isModal, setModal] = useState(false);
-  const [isContent, setContent] = useState();
+  // const [isContent, setContent] = useState();
   const [isTitle, setTitle] = useState();
   const [isHeader, setHeader] = useState(false);
   const onClose = () => setModal(false);
   const dispatch = useDispatch();
 
   const sendComment = (id, newMessageComment) =>
-    dispatch(sendMessageComment(id, newMessageComment));
+      dispatch(sendMessageComment(id, newMessageComment));
   const nickName = props.nickName || "NoName";
   const id = props.id;
 
   const commentEl = props.comments.map((el) => {
     return (
-      <div className={s.comment}>
-        <span className={s.commentator}>{el.nickname}</span>
-        <span className={s.message}>{el.message}</span>
-      </div>
+        <div className={s.comment}>
+          <span className={s.commentator}>{el.nickname}</span>
+          <span className={s.message}>{el.message}</span>
+        </div>
     );
   });
 
   const onCard = () => {
     setTitle("");
-    setContent(
-      <ModalCard
-        sendMessageComment={sendComment}
-        id={id}
-        commentEl={commentEl}
-        nickName={nickName}
-        img={props.img}
-      />
-    );
+    // setContent(
+    //   <ModalCard
+    //     sendMessageComment={sendComment}
+    //     id={id}
+    //     commentEl={commentEl}
+    //     nickName={nickName}
+    //     img={props.img}
+    //   />
+    // );
     setModal(true);
     setHeader(false);
   };
 
   return (
-    <>
-      <article className={s.card}>
-        <div className={s.header}>
-          <NavLink to="/profile/id">{nickName}</NavLink>
-          <button className={s.btnMore}>
-            <span className={s.kebabMenu}></span>
+      <>
+        <article className={s.card}>
+          <div className={s.header}>
+            <NavLink to="/profile/id">{nickName}</NavLink>
+            <button className={s.btnMore}>
+              <span className={s.kebabMenu}></span>
+            </button>
+          </div>
+          <div className={s.photos}>
+            {/* Сделать обработку изображения до квадрата */}
+            <img className={s.photosImg} src={props.img} alt="image" />
+          </div>
+          <BlockAction onCard={onCard} />
+          <div className={s.description}>{props.description}</div>
+          <button className={s.comments} onClick={onCard}>
+            Посмотреть все коментарии
           </button>
-        </div>
-        <div className={s.photos}>
-          {/* Сделать обработку изображения до квадрата */}
-          <img className={s.photosImg} src={props.img} alt="image" />
-        </div>
-        <BlockAction onCard={onCard} />
-        <div className={s.description}>{props.description}</div>
-        <button className={s.comments} onClick={onCard}>
-          Посмотреть все коментарии
-        </button>
-        <div className={s.date}>{props.date}</div>
-        <WriteComment sendMessageComment={sendComment} id={props.id} />
-      </article>
-      <Modal
-        visible={isModal}
-        title={isTitle}
-        content={isContent}
-        onClose={onClose}
-        isHeader={isHeader}
-      />
-    </>
+          <div className={s.date}>{props.date}</div>
+          <WriteComment sendMessageComment={sendComment} id={props.id} />
+        </article>
+        <Modal
+            visible={isModal}
+            title={isTitle}
+            // content={isContent}
+            onClose={onClose}
+            isHeader={isHeader}
+            sendMessageComment={sendComment}
+            id={id}
+            commentEl={commentEl}
+            nickName={nickName}
+            img={props.img}
+        />
+      </>
   );
 };
 
